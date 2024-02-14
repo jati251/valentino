7541f084e4ac6cee4f2e1c5633df3af152384915
Jati Suryo Buwono <jati.buwono@ist.id>
2024-02-14 17:55:53 +0700

    added many

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,108 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React, { useEffect, useRef, useState } from 'react';
+import logo from './dilla.png';
 
-const inter = Inter({ subsets: ["latin"] });
+const loveMessages = [
+  "You make my heart skip a beat.",
+  "I love you more than words can express.",
+  "You are the light of my life.",
+  "You mean everything to me.",
+  "My love for you grows stronger every day.",
+  "You are my soulmate.",
+  "I am grateful for your love.",
+  "You are the best thing that ever happened to me.",
+  "I can't imagine my life without you.",
+  "You complete me.",
+  "I cherish every moment with you."
+];
+const noMessage = [
+  'No',
+  "you sure ?",
+  "are you really sure ?",
+  "think again",
+  "last change",
+  "come on  :(",
+  "im sad , please dont",
+  "how dare you",
+  "i hate you",
+];
 
-export default function Home() {
+const getRandomMessage = () => {
+  const randomIndex = Math.floor(Math.random() * loveMessages.length);
+  return loveMessages[randomIndex];
+};
+
+const ValentinePage = () => {
+  const [message, setMessage] = useState('');
+  const [title, setTitle] = useState('Will you be my Valentine?');
+  const [play, setPlay] = useState(false);
+  const [click, setclick] = useState(0);
+  const [next, isnext] = useState(false);
+  const [noClicks, setNoClicks] = useState(0);
+  const [showHearts, setShowHearts] = useState(false);
+
+  const handleYesClick = () => {
+    setShowHearts(true);
+    setTitle('Yeayy, thank you so much :"""DDD')
+  };
+
+  const generateMessage = () => {
+    const randomMessage = getRandomMessage();
+    setMessage(randomMessage);
+    setPlay(true)
+    setclick(click + 1)
+  };
+
+  const handleNoClick = () => {
+    if (noClicks < noMessage.length) {
+      setNoClicks(noClicks + 1);
+    }
+
+  };
+
+  useEffect(() => {
+    const audioElement = document.getElementById('audioPlayer');
+    if (audioElement && play) {
+      audioElement.play();
+    }
+  }, [play]);
+
+  useEffect(() => {
+    if (click >= 4) {
+      isnext(true)
+    }
+  }, [click])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-pink-400 min-h-screen flex flex-col items-center justify-center">
+      <img src='https://i.ibb.co/KhDFqTN/IMG-8254-Background-Removed.png' className="w-64 h-auto" alt="logo" />
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Happy Valentine's Day!</h1>
+        {next ? <div>
+          <p className="text-lg mb-8">{title}</p>
+          <div className="flex justify-center gap-4">
+            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" style={{ fontSize: 16 + noClicks * 10 }}onClick={handleYesClick} >Yes</button>
+            <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" onClick={handleNoClick} >{noMessage[noClicks]} </button>
+          </div>
         </div>
+          :
+          <div>
+            <p className="text-lg mb-8">Welcome to our Valentine's Day page Dilla. Click the button below to generate a love message!</p>
+            <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" onClick={generateMessage}>Generate Love Message</button>
+            <p className="text-lg mt-4 animate-bounce">{message}</p></div>}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {showHearts && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <img src='https://static.vecteezy.com/system/resources/previews/010/331/422/original/little-hearts-clipart-with-transparent-background-for-decoration-of-art-file-free-png.png' alt="Heart" className="w-16 h-16 animate-spin" />
+          <img src='https://static.vecteezy.com/system/resources/previews/010/331/422/original/little-hearts-clipart-with-transparent-background-for-decoration-of-art-file-free-png.png' alt="Heart" className="w-16 h-16 animate-spin" />
+          <img src='https://static.vecteezy.com/system/resources/previews/010/331/422/original/little-hearts-clipart-with-transparent-background-for-decoration-of-art-file-free-png.png' alt="Heart" className="w-16 h-16 animate-spin" />
+        </div>
+      )}
+      <audio id="audioPlayer" autoPlay>
+        <source src="/audio.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </div >
   );
-}
+};
+
+export default ValentinePage;
